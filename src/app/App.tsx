0572aca8813fb20ec914a00d694971b6915dcd11
svensktutby <svensk.tut.by@gmail.com>
2021-04-14 15:00:22 +0300

    feat: add common components and libraries

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import s from './App.module.scss'
+
+export const App = () => {
+
+  return (
+    <div className={s.app}>
+      <div className={s.container}><h1>Тестовое задание <span>ООО Айсофт Сервисез</span></h1></div>
+    </div>
+  );
+};
